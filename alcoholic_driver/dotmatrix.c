8d682236cc1b5909302b50da16e7d86a93b58be3
woguddl <64778086+woguddl@users.noreply.github.com>
2020-11-30 21:39:33 +0900

    dotm driver

---
+++ alcoholic_driver/dotmatrix.c
@@ -0,0 +1,186 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/delay.h>
+#include <asm/ioctl.h>
+
+#define DRIVER_AUTHOR   "Alcoholic"
+#define DRIVER_DESC     "driver for dotmatrix"
+
+#define DOTM_NAME               "dotmatrix"
+#define DOTM_MODULES_VERSION    "dotmatrix V1.0"
+#define DOTM_ADDR               0x210
+
+#define DOTM_MAGIC              0xBC
+#define DOTM_SET_CLEAR          _IOW(DOTM_MAGIC, 0, int)
+#define DOTM_SPIN               _IOW(DOTM_MAGIC, 1, int)
+
+
+#define DOT_WIDTH       8 
+#define DOT_HEIGHT      10
+unsigned char buffer[10] = {0, };
+unsigned char current_word[10] = {0, };
+int position = 0;
+int word_len = 0;
+unsigned char updown_buffer[10] = {0, };
+
+extern ssize_t iom_fpga_itf_read(unsigned int addr);
+extern ssize_t iom_fpga_itf_write(unsigned int addr, unsigned short int value);
+
+static int dotm_in_use = 0;
+
+unsigned char dotm_fontmap_spin[6][10] = {
+        {0x7f,0x40,0x7f,0x08,0x7f,0x00,0x7f,0x0e,0x38,0x7f}, // dol
+        {0x00,0x02,0x32,0x4a,0x4b,0x4a,0x32,0x02,0x00,0x00}, // ah
+        {0x7a,0x0a,0x13,0x22,0x42,0x00,0x20,0x20,0x3e,0x00}, // gan
+        {0x00,0x02,0x7a,0x42,0x43,0x42,0x7a,0x02,0x00,0x00}, // da
+        {0x00,0x00,0x00,0x30,0x49,0x06,0x00,0x00,0x00,0x00}, // ~
+        {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}  // empty
+};
+unsigned char dotm_fontmap_empty[10] = {
+        0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+
+void clearBuf(unsigned char *buffer, int len)
+{
+        int i;
+        for(i=0; i<len; i++){
+                buffer[i]=0;
+        }
+}
+
+
+int dotm_open(struct inode *pinode, struct file *pfile)
+{
+        if (dotm_in_use != 0){
+                return -EBUSY;
+        }
+
+        dotm_in_use = 1;
+
+        return 0;
+}
+
+int dotm_release(struct inode *pinode, struct file *pfile)
+{
+        dotm_in_use = 0;
+        return 0;
+}
+
+ssize_t dotm_write(struct file *pinode, const char *gdata, size_t len, loff_t *off_what)
+{
+        int ret, i;
+        unsigned short wordvalue;
+        const char *tmp = NULL;
+        position=0;
+        tmp = gdata;
+
+        ret = copy_from_user(buffer, tmp, len);
+
+        if (ret) {
+                return -EFAULT;
+        }
+
+        for (i=0; i< 10; i++)
+        {
+                wordvalue = dotm_fontmap_empty[i] & 0x7F;
+                iom_fpga_itf_write((unsigned int) DOTM_ADDR+(i*2), wordvalue);
+                current_word[i] = dotm_fontmap_empty[i] & 0x7F;
+        }
+
+
+
+        return len;
+}
+
+static long dotm_ioctl(struct file *pinode, unsigned int cmd, unsigned long data)
+{
+        int i;
+        unsigned short wordvalue;
+        unsigned char next_word[10];
+        int next_word_index;
+
+        switch (cmd){
+
+        case DOTM_SET_CLEAR:
+                for (i=0; i<10;i++){
+                        wordvalue = dotm_fontmap_empty[i] & 0x7F;
+                        iom_fpga_itf_write((unsigned int) DOTM_ADDR+(i*2), wordvalue);
+                }
+
+                for (i=0; i<10; i++) {
+                        updown_buffer[i] = 0;
+                }
+
+                break;
+
+        case DOTM_SPIN:
+                for(i=0;i<10;i++){
+                        next_word[i] = 0;
+                }
+
+                for(i=0;i<10; i++){
+                        current_word[i] <<= 1;
+                        current_word[i] &= 0x7F;
+                }
+
+                if(position > 0 || (position / DOT_WIDTH) < word_len) {
+                        next_word_index = (position / DOT_WIDTH);
+
+                        if(position % DOT_WIDTH != 0){
+                                for(i=0;i<10;i++){
+                                        next_word[i] = dotm_fontmap_spin[buffer[next_word_index]][i];
+                                        next_word[i] >>= DOT_WIDTH - (position % DOT_WIDTH)-1;
+                                        next_word[i] &= 0x01;
+                                }
+                        }
+                }
+
+                for(i=0;i<10;i++){
+                        current_word[i] = (current_word[i] | next_word[i]) & 0x7F;
+                        wordvalue = current_word[i];
+                        iom_fpga_itf_write((unsigned int) DOTM_ADDR+(i*2), wordvalue);
+                }
+                position++;
+                break;
+
+        }
+        return 0;
+}
+
+static struct file_operations dotm_fops = {
+        .owner  = THIS_MODULE,
+        .open   = dotm_open,
+        .write  = dotm_write,
+        .unlocked_ioctl = dotm_ioctl,
+        .release= dotm_release,
+};
+
+static struct miscdevice dotm_driver = {
+        .fops   = &dotm_fops,
+        .name   = DOTM_NAME,
+        .minor  = MISC_DYNAMIC_MINOR,
+};
+
+static int dot_init(void)
+{
+        misc_register(&dotm_driver);
+        printk("init module dot\n");
+        return 0;
+}
+
+static void dot_exit(void)
+{
+        misc_deregister(&dotm_driver);
+        printk("exit module dot\n");
+}
+
+module_init(dot_init);
+module_exit(dot_exit);
+
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_LICENSE("Dual BSD/GPL");
---
+++ alcoholic_driver/dottest.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define DOTM_MAGIC              0xBC
+#define DOTM_SET_CLEAR          _IOW(DOTM_MAGIC, 0, int)
+#define DOTM_SPIN               _IOW(DOTM_MAGIC, 1, int)
+
+unsigned char quit = 0;
+void user_signal1(int sig)
+{
+        quit = 1;
+}
+
+int main(int argc, char** argv)
+{
+        int fd, i, j, size;
+        int num;
+        unsigned char buffer[6];
+
+        fd = open("/dev/dotmatrix",O_RDWR);
+
+        if (fd < 0){
+                printf("Device Open Error\n");
+                close(fd);
+                return -1;
+        }
+
+        (void)signal(SIGINT, user_signal1);
+
+        if(strcmp(argv[1], "spin") == 0) {
+                for(i=0; i<6; i++) {
+                        buffer[i] = i;
+                }
+                buffer[6]='\0';
+
+                for(i=0; i<2; i++) {
+                        write(fd, buffer, 5*sizeof(int));
+
+                        for(j=0; j<48; j++) {
+                                ioctl(fd, DOTM_SPIN, NULL, _IOC_SIZE(DOTM_SPIN));
+                        }
+                }
+                ioctl(fd, DOTM_SET_CLEAR, NULL);
+        }
+
+        close(fd);
+}
