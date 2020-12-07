8a1da46a7ac7643eeb420db4dc80853b1dd957de
jomingyu <owlsogul@gmail.com>
2020-12-07 20:55:44 +0900

    add comment at button callback

--- alchoholic_ver1/11_4_HuntingBird2/app/src/main/java/com/company/my/alchoholic/sensor/ButtonCallback.java
+++ alchoholic_ver1/11_4_HuntingBird2/app/src/main/java/com/company/my/alchoholic/sensor/ButtonCallback.java
@@ -1,7 +1,11 @@
 package com.company.my.alchoholic.sensor;
 
 public interface ButtonCallback {
-
+    /**
+     * 버튼값을 지속적으로 받아오는 메소드
+     * 0또는 1이 값이 넘어와진다.
+     * @param value 버튼이 눌렸으면 1 안눌렸으면 0
+     */
     public void onButtonClick(int value);
 
 }
