c1bde106be111a4bc4194b872d0a9e7e0fed32a0
jomingyu <owlsogul@gmail.com>
2020-12-08 13:40:49 +0900

    버블 안쓰는 부분 삭제

--- alchoholic_ver1/11_4_HuntingBird2/app/src/main/java/com/company/my/alchoholic/Bubble_MainActivity.java
+++ alchoholic_ver1/11_4_HuntingBird2/app/src/main/java/com/company/my/alchoholic/Bubble_MainActivity.java
@@ -19,25 +19,11 @@ public class Bubble_MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//스크린 회전
         setContentView(R.layout.bubble_activity_main);
 
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-      //  setSupportActionBar(toolbar);
-
         // Statusbar 감추기
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // 타이틀 감추기
-        // getSupportActionBar().hide();
         setTitle("비눗방울 터뜨리기 - Thread");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // ((GameView) findViewById(R.id.gameView)).mThread = null;
-                finish();
-                // android.os.Process.killProcess(android.os.Process.myPid());
-            }
-        });
         dbAdapter = new myDBAdapter(this);
         new Handler().postDelayed(new Runnable()
         {
